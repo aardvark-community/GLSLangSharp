bbc4378cf9ab202ac3ed6a0582236dbd764c1995
krauthaufen <krauthaufen@awx.at>
2015-09-23 07:34:39 +0200

    renamed Type to ShaderType (avoiding name clashes with System.Type); added ShaderType module for printing glsl-style type-names/decorations; interface parameters are now sorted by location/set/binding/name depending on their StorageClass;

--- src/GLSLangSharp/SpirVReflection.fs
+++ src/GLSLangSharp/SpirVReflection.fs
@@ -3,24 +3,119 @@
 open System.IO
 
 type Width = int
-type Type =
+type ShaderType =
     | Int of Width * bool
     | Float of Width
     | Bool 
-    | Vector of Type * int
-    | Matrix of Type * int 
-    | Ptr of Type
-    | Array of Type * int
-    | Image of dim : int * isDepth : bool * isArray : bool * isMultisampled : bool 
+    | Vector of ShaderType * int
+    | Matrix of ShaderType * int 
+    | Ptr of ShaderType
+    | Array of ShaderType * int
+    | Image of sampledType : ShaderType * dim : Dim * isDepth : bool * isArray : bool * isMultisampled : bool 
     | Sampler
-    | Struct of list<Type * string * list<Decoration * uint32[]>>
+    | Struct of string * list<ShaderType * string * list<Decoration * uint32[]>>
     | Void
   
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module ShaderType =
+
+    let glslDecoration (decorations : list<Decoration * uint32[]>) =
+        if List.isEmpty decorations then
+            ""
+        else
+            let decorations =
+                decorations |> List.map (fun t ->
+                    match t with
+                        | Decoration.Location, [|loc|] -> sprintf "location = %d" (int loc)
+                        | Decoration.Binding, [|b|] -> sprintf "binding = %d" (int b)
+                        | Decoration.DescriptorSet, [|s|] -> sprintf "set = %d" (int s)
+                        | Decoration.Index, [|i|] -> sprintf "index = %d" (int i)
+                        | Decoration.BuiltIn, [|id|] -> sprintf "builtin = %A" (unbox<BuiltIn> id)
+
+                        | Decoration.Nonreadable, _ -> "write_only"
+                        | Decoration.Nonwritable, _ -> "read_only"
+                        | Decoration.NoStaticUse, _ -> "nostaticuse"
+                        | Decoration.RowMajor, _ -> "row_major"
+                        | Decoration.ColMajor, _ -> "col_major"
+                        | Decoration.Noperspective, _ -> "noperspective"
+                        | Decoration.Flat, _ -> "flat"
+
+                        | d, [||] -> sprintf "%A" d
+                        | d, args -> sprintf "%A = { %s }" d (args |> Array.map string |> String.concat ", ")
+                )
+            decorations |> String.concat ", " |> sprintf "layout(%s)"
+
+    let rec glslName (t : ShaderType) =
+        match t with
+            | Ptr t -> glslName t
+            | Array(t, l) -> sprintf "%s[%d]" (glslName t) l
+
+            | Bool -> "bool"
+            | Int(32, true) -> "int"
+            | Int(32, false) -> "uint"
+            | Float 32 -> "float"
+            | Float 64 -> "double"
+
+            | Vector(Bool, d) -> sprintf "bvec%d" d
+            | Vector(Int(32, true), d) -> sprintf "ivec%d" d
+            | Vector(Int(32, false), d) -> sprintf "uvec%d" d
+            | Vector(Float 32, d) -> sprintf "vec%d" d
+            | Vector(Float 64, d) -> sprintf "dvec%d" d
+
+            | Matrix(Vector(Float 32, r), c) -> 
+                if r = c then sprintf "mat%d" r
+                else sprintf "mat%dx%d" r c
+
+            | Matrix(Vector(Float 64, r), c) -> 
+                if r = c then sprintf "dmat%d" r
+                else sprintf "dmat%dx%d" r c
+
+            | Image(st,dim,d,a,m) -> 
+                let st =
+                    match st with
+                        | Float 32 -> ""
+                        | Int(32, true) -> "i"
+                        | Int(32, false) -> "u"
+                        | _ -> "g"
+
+                let dim =
+                    match dim with
+                        | Dim.Dim1D -> "1D"
+                        | Dim.Dim2D -> "2D"
+                        | Dim.Dim3D -> "3D"
+                        | Dim.DimBuffer -> "Buffer"
+                        | Dim.DimCube -> "Cube"
+                        | Dim.DimRect -> "Rect"
+                        | _ -> failwithf "unsupported sampler dimension: %A" dim
+                let d = if d then "Shadow" else ""
+                let a = if a then "Array" else ""
+                let m = if m then "MS" else ""
+                st + "sampler" + dim + m + a + d
+
+
+
+            | Struct(name, fields) ->
+                
+                let strs =
+                    fields |> List.map (fun (t,n,dec) ->
+                        let t = glslName t
+                        let layout = glslDecoration dec
+
+                        if layout.Length = 0 then
+                            sprintf "    %s %s;" t n
+                        else
+                            sprintf "    %s %s %s;" layout t n
+                    )
+
+                strs |> String.concat "\r\n" |> sprintf "%s {\r\n%s\r\n }" name
+
+            | _ -> sprintf "unknown type: %A" t
 
 type Parameter = 
     { 
         paramName : string
-        paramType : Type
+        paramType : ShaderType
         paramDecorations : list<Decoration * uint32[]> 
     }
 
@@ -54,9 +149,14 @@ module Parameter =
                 | _ -> None
         )
 
+
+
     let inline paramName (p : Parameter) = p.paramName
     let inline paramType (p : Parameter) = p.paramType
     let inline paramDecorations (p : Parameter) = p.paramDecorations
+
+
+
 
 
 
@@ -69,6 +169,7 @@ type ShaderInterface =
         uniforms : list<Parameter>
         images : list<Parameter> 
     }
+
 
 
 
@@ -160,12 +261,13 @@ module SpirVReflection =
                         let! colType = typeForId colType
                         return Matrix(colType,int colCount)
                     | TypeImage(resId,sampledType,dim,depth, arrayed, ms, sampled,format,access) ->
-                        return Image(int dim, depth,arrayed,ms)
+                        let! sampledType = typeForId sampledType
+                        return Image(sampledType, unbox<Dim> dim, depth,arrayed,ms)
                     | TypeSampler(_) -> return Sampler
                     | TypeArray(_,elem,len) -> 
                         let! elem = typeForId elem
                         return Array(elem,int len)
-                    | TypeStruct(_, fields) ->
+                    | TypeStruct(res, fields) ->
                         let! fieldTypes = fields |> Array.toList |> List.mapOption typeForId
 
                         let fieldDecorations =
@@ -182,7 +284,9 @@ module SpirVReflection =
                                     | _ -> failwith "no field name"
                             )
 
-                        return Struct (List.zip3 fieldTypes fieldNames fieldDecorations)
+                        let! n = Map.tryFind res names
+
+                        return Struct (n, List.zip3 fieldTypes fieldNames fieldDecorations)
 
                     | TypePointer(_,storageClass,baseType) ->
                         let! baseType = typeForId baseType
@@ -216,13 +320,31 @@ module SpirVReflection =
                     match p with
                         | ci, n, t, d when ci = c -> Some { paramName = n; paramType = t; paramDecorations = d }
                         | _ -> None
-                )
+                   )
 
 
+        let ioSort (p : Parameter) =
+            match Parameter.tryGetLocation p with
+                | Some l -> (l, p.paramName)
+                | None -> (System.Int32.MaxValue, p.paramName)
+
+        let uniformSort (p : Parameter) =
+            let set = 
+                match Parameter.tryGetDescriptorSet p with
+                    | Some l -> l
+                    | None -> System.Int32.MaxValue
+   
+            let binding = 
+                match Parameter.tryGetDescriptorSet p with
+                    | Some l -> l
+                    | None -> System.Int32.MaxValue
+
+            (set, binding, p.paramName)
+        
         let (execModel, entryPoint) = instructions |> List.pick (function EntryPoint(m,_,name) -> Some (m,name) | _ -> None)
-        let inputs = extractParamteters StorageClass.Input
-        let outputs = extractParamteters StorageClass.Output
-        let uniforms = extractParamteters StorageClass.Uniform
-        let images = extractParamteters StorageClass.Image
+        let inputs = extractParamteters StorageClass.Input |> List.sortBy ioSort
+        let outputs = extractParamteters StorageClass.Output |> List.sortBy ioSort
+        let uniforms = extractParamteters StorageClass.Uniform |> List.sortBy uniformSort
+        let images = extractParamteters StorageClass.Image |> List.sortBy uniformSort
 
         { executionModel = execModel; entryPoint = entryPoint ;inputs = inputs; outputs = outputs; uniforms = uniforms; images = images }
