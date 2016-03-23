c75c9a9a594301be58be65739d0c487310ef24e9
krauthaufen <krauthaufen@awx.at>
2016-03-23 18:46:17 +0100

    updated build script and f# version;

--- build.fsx
+++ build.fsx
@@ -1,21 +1,26 @@
-#I @"packages/FAKE/tools/"
-#I @"packages/Paket.Core/lib/net45"
-#I @"bin"
-#r @"System.Xml.Linq"
-#r @"FakeLib.dll"
-#r @"Paket.Core.dll"
-#r @"Aardvark.Fake.dll"
-
+#load @"paket-files/build/vrvis/Aardvark.Fake/DefaultSetup.fsx"
 
 open Fake
 open System
 open System.IO
 open System.Diagnostics
 open Aardvark.Fake
+open Fake.Testing
+
 
 do Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-DefaultTargets.install ["src/GLSLang.sln"]
+DefaultSetup.install ["src/GLSLang.sln"]
 
-// start build
-RunTargetOrDefault "Default"
+Target "Tests" (fun () ->
+    NUnit3 (fun p -> { p with OutputDir = "tests.out" })  (!! @"bin\Release\*.Tests.dll" ++ @"bin\Release\*.Tests.exe")
+)
+
+"Compile" ==> "Tests"
+
+#if DEBUG
+do System.Diagnostics.Debugger.Launch() |> ignore
+#endif
+
+
+entry()
