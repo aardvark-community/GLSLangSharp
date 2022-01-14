aeec058b1fe48c7e9484b66db1fba72b9650f1f0
krauthaufen <krauthaufen@awx.at>
2022-01-15 00:47:32 +0100

    separate OSX builds per architecture

--- buildnative.sh
+++ buildnative.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
 os=$(uname -s)
-echo $os
 if [ $os == "Darwin" ];
 then
-    cmake -DCMAKE_OSX_ARCHITECTURES=arm64 -S src/GLSLangNative -B bin/arm64
-    cmake --build bin/arm64 --config Release
-    cmake --install bin/arm64 --config Release
+    cmake -DCMAKE_OSX_ARCHITECTURES=$1 -S src/GLSLangNative -B bin/arm64
+    cmake --build bin/$1 --config Release
+    cmake --install bin/$1 --config Release
 
-    cmake -DCMAKE_OSX_ARCHITECTURES=x86_64 -S src/GLSLangNative -B bin/x86_64
-    cmake --build bin/x86_64 --config Release
-    cmake --install bin/x86_64 --config Release
 else
     cmake -S src/GLSLangNative -B bin/build
     cmake --build bin/build --config Release
