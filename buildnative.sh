fc85875e40147c1c19222ea10b1485897475736b
krauthaufen <krauthaufen@awx.at>
2022-01-15 23:43:40 +0100

    sync

--- buildnative.sh
+++ buildnative.sh
@@ -9,11 +9,11 @@ then
         arch=$(uname -m)
     fi
     cmake -DCMAKE_OSX_ARCHITECTURES=$arch -S src/GLSLangNative -B bin/$arch
-    cmake --build bin/$arch --config MinSizeRel
+    cmake --build bin/$arch --config MinSizeRel --target install/strip
     cmake --install bin/$arch --config MinSizeRel
 
 else
     cmake -S src/GLSLangNative -B bin/build
-    cmake --build bin/build --config Release
+    cmake --build bin/build --config Release --target install/strip
     cmake --install bin/build --config Release
 fi
