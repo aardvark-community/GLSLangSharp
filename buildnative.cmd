d0b2fde34557a33a9efb7f6b81a34ecc8e943ea8
Martin <mautner@vrvis.at>
2024-09-04 15:31:50 +0200

    Update native libraries

--- buildnative.cmd
+++ buildnative.cmd
@@ -1,4 +1,4 @@
 @echo off
-cmake -S src\GLSLangNative -B bin\build
+cmake -DCMAKE_BUILD_TYPE=Release -S src\GLSLangNative -B bin\build
 cmake --build bin\build --config MinSizeRel
 cmake --install bin\build --config MinSizeRel
